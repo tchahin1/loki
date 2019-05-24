bb58d6c79f363b447460527722b450a4fe4dd791
ilmalusija <ilmalusija>
2019-05-24 15:07:27 +0200

    Designed login screen

--- mobile/MojaEPBiH/screens/SignedOut/SignIn/SignIn.jsx
+++ mobile/MojaEPBiH/screens/SignedOut/SignIn/SignIn.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { View } from 'react-native';
 import {
-  Card, Button,
-} from 'react-native-elements';
+  View,
+  Text,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Keyboard,
+  Platform,
+} from 'react-native';
+import { Button } from 'react-native-elements';
 import { TextField } from 'react-native-material-textfield';
 import PropTypes from 'prop-types';
 
+import Colors from '../../../assets/colors/AppColorsEnum';
+import Background from '../../../assets/images/epbih.jpg';
+import createStyles from './SignIn.styles';
+
+
+const styles = createStyles();
+
 export default class SignIn extends React.Component {
     static navigationOptions = {
+      header: null,
       title: 'SIGN IN',
     };
 
@@ -18,26 +31,61 @@ export default class SignIn extends React.Component {
     render() {
       const { navigation } = this.props;
       return (
-        <View style={{ paddingVertical: 20 }}>
-          <Card>
-            <TextField label="Email address" animationDuration={400} tintColor="#16489b" />
-            <TextField secureTextEntry label="Password" animationDuration={400} tintColor="#16489b" />
+        <ImageBackground source={Background} style={styles.wrapper}>
 
+          <KeyboardAvoidingView
+            style={styles.wrapper}
+            behavior={Platform.OS === 'ios' ? 'padding' : null}
+          >
+            <View style={{ justifyContent: 'flex-end', flex: 1 }}>
+              <View style={styles.titleWrapper}>
+                <Text style={styles.title}>Moja EP</Text>
+              </View>
+              <View style={styles.inputsWrapper}>
+                <TextField
+                  label="E-mail"
+                  animationDuration={100}
+                  tintColor={Colors.PRIMARY_WHITE}
+                  textColor={Colors.PRIMARY_WHITE}
+                  baseColor={Colors.PRIMARY_WHITE}
+                  lineWidth={2}
+                  activeLineWidth={3.5}
+                  fontSize={18}
+                  labelFontSize={15}
+                />
+                <TextField
+                  secureTextEntry
+                  label="Šifra"
+                  animationDuration={100}
+                  tintColor={Colors.PRIMARY_WHITE}
+                  textColor={Colors.PRIMARY_WHITE}
+                  baseColor={Colors.PRIMARY_WHITE}
+                  lineWidth={2}
+                  activeLineWidth={3.5}
+                  fontSize={18}
+                  labelFontSize={15}
+                />
+              </View>
+              <View style={styles.btnWrapper}>
+                <Button
+                  buttonStyle={styles.btnSignIn}
+                  title="PRIJAVI SE"
+                  titleStyle={{ fontSize: 18 }}
+                  onPress={() => navigation.navigate('SignedIn')}
+                />
+              </View>
+              <View style={{ flex: 1 }} />
+            </View>
+          </KeyboardAvoidingView>
+          <View style={{ justifyContent: 'flex-end' }}>
             <Button
-              buttonStyle={{ marginTop: 20 }}
-              backgroundColor="#03A9F4"
-              title="SIGN IN"
-              onPress={() => navigation.navigate('SignedIn')}
-            />
-            <Button
-              buttonStyle={{ marginTop: 20 }}
-              backgroundColor="transparent"
-              textStyle={{ color: '#bcbec1' }}
-              title="SIGN UP"
+              type="clear"
+              title="REGISTRUJ SE"
+              titleStyle={styles.btnSignUpTitle}
               onPress={() => navigation.navigate('SignUp')}
             />
-          </Card>
-        </View>
+          </View>
+        </ImageBackground>
       );
     }
 }
---
+++ mobile/MojaEPBiH/screens/SignedOut/SignIn/SignIn.styles.js
@@ -0,0 +1,43 @@
+import { StyleSheet } from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+
+import Colors from '../../../assets/colors/AppColorsEnum';
+
+const styles = {
+  wrapper: {
+    flex: 1,
+  },
+  inputsWrapper: {
+    marginRight: wp('4%'),
+    marginLeft: wp('4%'),
+    marginTop: hp('5%'),
+  },
+  btnSignIn: {
+    backgroundColor: Colors.PRIMARY_BLUE,
+    marginTop: hp('5%'),
+    height: hp('8%'),
+  },
+  btnWrapper: {
+    marginRight: wp('4%'),
+    marginLeft: wp('4%'),
+  },
+  title: {
+    fontSize: 60,
+    color: Colors.PRIMARY_WHITE,
+  },
+  btnSignUpTitle: {
+    fontSize: 20,
+    color: Colors.NOTICE_COLOR,
+  },
+  titleWrapper: {
+    alignItems: 'center',
+    marginTop: hp('15%'),
+  },
+};
+
+export default function createStyles(overrides = {}) {
+  return StyleSheet.create({ ...styles, ...overrides });
+}
