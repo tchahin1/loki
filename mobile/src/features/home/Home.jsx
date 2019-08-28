d0677da930cb483fea3f103c76964b94f41279cb
unknown <tarek@pragmatio.com>
2019-08-28 09:39:03 +0200

    ADDED logic for recieving push notification

--- mobile/src/features/home/Home.jsx
+++ mobile/src/features/home/Home.jsx
@@ -6,6 +6,7 @@ import {
   View,
   ActivityIndicator,
 } from 'react-native';
+import { Notifications, Permissions } from 'expo';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { Header } from 'react-native-elements/src/index';
@@ -29,6 +30,8 @@ import { initializeElectricMeter } from '../electric-meter/ElectricMeterActions'
 
 const styles = createStyles();
 
+// const PUSH_ENDPOINT = 'https://your-server.com/users/push-token';
+
 class HomeScreen extends React.Component {
   static propTypes = {
     serverText: PropTypes.shape({}).isRequired,
@@ -37,6 +40,7 @@ class HomeScreen extends React.Component {
     receivedTextFromServer: PropTypes.func.isRequired,
     navigation: PropTypes.shape({}).isRequired,
     user: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
     LogoutUser: PropTypes.func.isRequired,
     InitializePlaceOfMeasurementModal: PropTypes.func.isRequired,
     InitializeElectricMeter: PropTypes.func.isRequired,
@@ -54,6 +58,10 @@ class HomeScreen extends React.Component {
 
   componentWillMount() {
     getToken().then(token => this.setState({ token }, () => this.fetchServerText()));
+  }
+
+  componentDidMount() {
+    this.registerForPushNotificationsAsync();
   }
 
   componentWillReceiveProps(nextProps) {
@@ -115,6 +123,52 @@ class HomeScreen extends React.Component {
     navigation.navigate(option.key);
   }
 
+  registerForPushNotificationsAsync = async () => {
+    const { username, user } = this.props;
+    const { status: existingStatus } = await Permissions.getAsync(
+      Permissions.NOTIFICATIONS,
+    );
+    let finalStatus = existingStatus;
+
+    // only ask if permissions have not already been determined, because
+    // iOS won't necessarily prompt the user a second time.
+    if (existingStatus !== 'granted') {
+      // Android remote notification permissions are granted during the app
+      // install, so this will only ask on iOS
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+
+    // Stop here if the user did not grant permissions
+    if (finalStatus !== 'granted') {
+      return;
+    }
+
+    // Get the token that uniquely identifies this device
+    const pushToken = await Notifications.getExpoPushTokenAsync();
+
+    // POST the token to your backend server from where you
+    // can retrieve it to send push notifications.
+    fetch(`${api}/notifications/push_user`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        privateKey: user,
+      },
+      body: JSON.stringify({
+        token: pushToken,
+        username,
+      }),
+    }).then((response) => {
+      if (response.ok) {
+        // console.log(response);
+      } else {
+        // console.log(response);
+      }
+    });
+  }
+
   render() {
     const { serverText } = this.props;
     const { current } = serverText;
@@ -173,6 +227,7 @@ class HomeScreen extends React.Component {
 const mapStateToProps = state => ({
   serverText: state.serverText,
   user: state.signIn.user,
+  username: state.signIn.id,
 });
 
 const mapDispatchToProps = dispatch => (
