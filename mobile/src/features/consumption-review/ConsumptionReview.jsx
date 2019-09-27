de1db46d644c443058798d3c2fe9f43b0e8c9f05
unknown <tarek@pragmatio.com>
2019-09-27 09:32:23 +0200

    ADDED charts in consumption review screen

--- mobile/src/features/consumption-review/ConsumptionReview.jsx
+++ mobile/src/features/consumption-review/ConsumptionReview.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
+import { LineChart } from 'react-native-chart-kit';
+import {
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
+import FlashMessage, { showMessage } from 'react-native-flash-message';
 import { Header } from 'react-native-elements/src/index';
 import PropTypes from 'prop-types';
 import MenuButton from '../../components/helpers/MenuButton';
@@ -9,7 +14,6 @@ import NotificationsModal from '../../components/helpers/NotificationsModal';
 import { onSignOut } from '../../../Auth';
 import createStyles from './ConsumptionReview.styles';
 import logoutUser from '../account/AccountActions';
-
 
 const styles = createStyles();
 
@@ -24,6 +28,11 @@ class ConsumptionReviewScreen extends React.Component {
     super(props);
     this.state = {
       openNotMod: false,
+      isVisible: false,
+      pointRect: {
+        x: 0, y: 0, width: 0, height: 0,
+      },
+      value: 0,
     };
   }
 
@@ -43,7 +52,12 @@ class ConsumptionReviewScreen extends React.Component {
 
   render() {
     const { navigation } = this.props;
-    const { openNotMod } = this.state;
+    const {
+      openNotMod, isVisible, pointRect, value,
+    } = this.state;
+
+    console.log(pointRect, isVisible, value);
+
     return (
       <View style={{ flex: 1 }}>
         <Header
@@ -58,7 +72,54 @@ class ConsumptionReviewScreen extends React.Component {
           )}
         />
         <View style={styles.container}>
-          <Text>ConsumptionReviewScreen</Text>
+          <Text>Line Chart</Text>
+          <View style={{ marginLeft: wp('-8%') }}>
+            <LineChart
+              data={{
+                labels: ['Jan', 'Feb', 'Mart', 'April', 'Maj', 'Juni',
+                  'Juli', 'Aug', 'Sept', 'Okt', 'Nov', 'Dec'],
+                datasets: [{
+                  data: [
+                    78, 23, 44, 56, 88, 23, 12, 18, 22, 65, 73, 10,
+                  ],
+                  color: (opacity = 0.2) => `rgba(0, 255, 0, ${opacity})`,
+                },
+                {
+                  data: [
+                    31, 44, 55, 61, 32, 13, 75, 51, 91, 33, 12, 1,
+                  ],
+                  color: (opacity = 0.2) => `rgba(0, 0, 255, ${opacity})`,
+                }],
+              }}
+              width={Dimensions.get('window').width} // from react-native
+              height={220}
+              chartConfig={{
+                backgroundColor: 'white',
+                backgroundGradientFrom: 'white',
+                backgroundGradientTo: 'white',
+                decimalPlaces: 0, // optional, defaults to 2dp
+                color: (opacity = 0.3) => `rgba(0, 0, 0, ${opacity})`,
+                style: {
+                  borderRadius: 16,
+                },
+              }}
+              style={{
+                marginVertical: 8,
+                borderRadius: 16,
+              }}
+              onDataPointClick={obj => showMessage({
+                message: obj.getColor() === 'rgba(0, 255, 0, 0.2)' ? 'Velika tarifa' : 'Mala tarifa',
+                description: `${obj.value}kWh`,
+                type: 'info',
+                position: 'center',
+                floating: true,
+                icon: 'info',
+                backgroundColor: obj.getColor(0.9),
+              })
+            }
+            />
+            <FlashMessage duration={1000} style={{ width: wp('90%'), marginLeft: wp('10%') }} />
+          </View>
           <NotificationsModal
             animationType="fade"
             transparent
