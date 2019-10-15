9041f057ce4c1a7eca8149239946153d2ea54f9b
unknown <tarek@pragmatio.com>
2019-10-15 08:11:51 +0200

    FINISHED the design of billing report feature

--- mobile/src/features/account/Account.jsx
+++ mobile/src/features/account/Account.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
-import { Header } from 'react-native-elements/src/index';
+import {
+  View, Text, Picker, TouchableOpacity, ScrollView,
+} from 'react-native';
+import { Header, Icon } from 'react-native-elements/src/index';
+import _ from 'lodash';
+import {
+  Table, Row,
+} from 'react-native-table-component';
 import PropTypes from 'prop-types';
 import MenuButton from '../../components/helpers/MenuButton';
 import NotificationsButton from '../../components/helpers/NotificationsButton';
 import NotificationsModal from '../../components/helpers/NotificationsModal';
+import PlaceOfMeasurementModal from '../../components/helpers/PlaceOfMeasurementModal';
+import Colors from '../../assets/colors/AppColorsEnum';
 import { onSignOut } from '../../../Auth';
 import createStyles from './Account.styles';
-
 import logoutUser from './AccountActions';
+import { placeChanged } from '../electric-meter/ElectricMeterActions';
 
 const styles = createStyles();
 
@@ -18,12 +26,19 @@ class AccountScreen extends React.Component {
     navigation: PropTypes.shape({}).isRequired,
     user: PropTypes.string.isRequired,
     LogoutUser: PropTypes.func.isRequired,
+    places: PropTypes.instanceOf(Array).isRequired,
+    PlaceChanged: PropTypes.func.isRequired,
+    selectedPlace: PropTypes.string.isRequired,
   };
 
   constructor(props) {
     super(props);
     this.state = {
       openNotMod: false,
+      flexStyle: 2 / 3,
+      openPOM: false,
+      tableHead: ['Head', 'Head2', 'Head3', 'Head4', 'Head5', 'Head6', 'Head7', 'Head8', 'Head9'],
+      widthArr: [40, 60, 80, 100, 120, 140, 160, 180, 200],
     };
   }
 
@@ -41,9 +56,27 @@ class AccountScreen extends React.Component {
     LogoutUser();
   }
 
+  onPlaceChanged(item) {
+    const { PlaceChanged } = this.props;
+
+    PlaceChanged(item);
+  }
+
   render() {
-    const { navigation } = this.props;
-    const { openNotMod } = this.state;
+    const { navigation, places, selectedPlace } = this.props;
+    const {
+      openNotMod, openPOM, flexStyle, widthArr, tableHead,
+    } = this.state;
+
+    const tableData = [];
+    for (let i = 0; i < 30; i += 1) {
+      const rowData = [];
+      for (let j = 0; j < 9; j += 1) {
+        rowData.push(`${i}${j}`);
+      }
+      tableData.push(rowData);
+    }
+
     return (
       <View style={{ flex: 1 }}>
         <Header
@@ -73,7 +106,7 @@ class AccountScreen extends React.Component {
                 />
               ))}
             </Picker>
-            <TouchableOpacity onPress={() => this.setState({ openPOM: true, previousPOM: true })}>
+            <TouchableOpacity onPress={() => this.setState({ openPOM: true })}>
               <Icon
                 type="ionicon"
                 name="ios-add"
@@ -83,8 +116,34 @@ class AccountScreen extends React.Component {
             </TouchableOpacity>
           </View>
         </View>
-        <View style={styles.container}>
-          <Text>AccountScreen</Text>
+        <View style={styles.container} flexStyle={flexStyle}>
+          <ScrollView horizontal>
+            <View>
+              <Table borderStyle={{ borderWidth: 1, borderColor: '#C1C0B9' }}>
+                <Row
+                  data={tableHead}
+                  widthArr={widthArr}
+                  style={styles.tableHeader}
+                  textStyle={styles.text}
+                />
+              </Table>
+              <ScrollView style={styles.dataWrapper}>
+                <Table borderStyle={{ borderWidth: 1, borderColor: '#C1C0B9' }}>
+                  {
+                    tableData.map((rowData, index) => (
+                      <Row
+                        key={1}
+                        data={rowData}
+                        widthArr={widthArr}
+                        style={[styles.row, index % 2 && { backgroundColor: '#F7F6E7' }]}
+                        textStyle={styles.tableText}
+                      />
+                    ))
+                  }
+                </Table>
+              </ScrollView>
+            </View>
+          </ScrollView>
           <NotificationsModal
             animationType="fade"
             transparent
@@ -93,6 +152,11 @@ class AccountScreen extends React.Component {
             onSignOutPress={this.onSignOutPressed}
           />
         </View>
+        <PlaceOfMeasurementModal
+          visible={openPOM}
+          onRequestClose={() => {}}
+          toggle={() => this.setState({ openPOM: false })}
+        />
       </View>
     );
   }
@@ -100,6 +164,10 @@ class AccountScreen extends React.Component {
 
 const mapStateToProps = state => ({
   user: state.signIn.user,
+  places: _.map(state.measurementPlaceModal.places, val => ({ ...val })),
+  selectedPlace: state.electricMeter.selectedPlace,
 });
 
-export default connect(mapStateToProps, { LogoutUser: logoutUser })(AccountScreen);
+export default connect(mapStateToProps, {
+  LogoutUser: logoutUser, PlaceChanged: placeChanged,
+})(AccountScreen);
--- mobile/src/features/account/Account.styles.js
+++ mobile/src/features/account/Account.styles.js
@@ -1,5 +1,8 @@
 import { StyleSheet } from 'react-native';
-
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 import Colors from '../../assets/colors/AppColorsEnum';
 
 const styles = {
@@ -13,6 +16,47 @@ const styles = {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    padding: 16,
+    paddingTop: 30,
+    backgroundColor: '#fff',
+  },
+  tableHeader: {
+    height: 50,
+    backgroundColor: '#537791',
+  },
+  tableText: {
+    textAlign: 'center',
+    fontWeight: '100',
+  },
+  dataWrapper: {
+    marginTop: -1,
+  },
+  row: {
+    height: 40,
+    backgroundColor: '#E7E6E1',
+  },
+  firstPart: {
+    flex: 1,
+    marginHorizontal: hp('2%'),
+    marginTop: wp('4%'),
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: wp('4%'),
+    marginTop: hp('6%'),
+  },
+  picker: {
+    width: wp('60%'),
+    height: hp('6%'),
+  },
+  metricPlacesContainer: {
+    flex: 1 / 3,
+    borderWidth: 1,
+    marginHorizontal: hp('1%'),
+    marginVertical: wp('4%'),
   },
 };
 
